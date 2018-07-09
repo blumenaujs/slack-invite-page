0d612e7bd65bb12308522a02b04b3da9f9a22d36
Marcos Vinicius <marcosvcontato.mv@gmail.com>
2018-07-09 16:33:35 -0300

    fix

--- src/screens/invite.js
+++ src/screens/invite.js
@@ -1,62 +1,61 @@
-import React, { PureComponent } from 'react'
-import styled from 'styled-components'
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import React, { PureComponent } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { toast } from "react-toastify";
 
-import Title from './title'
-import InviteForm from './invite-form'
-import Footer from './footer'
-import { API_URL, INVITES_SUFIX } from '../utils/constants'
+import Title from "./title";
+import InviteForm from "./invite-form";
+import Footer from "./footer";
+import { API_URL, INVITES_SUFIX } from "../utils/constants";
 
 const StyledInvite = styled.div`
-  width:100%;
-  height:100%;
-  display:flex;
-  flex-flow:column wrap;
-  align-items:center;
-  justify-content:center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-flow: column wrap;
+  align-items: center;
+  justify-content: center;
 
-  >.content{
+  > .content {
     padding: 3rem 0;
-    width:100%;
-    display:flex;
+    width: 100%;
+    display: flex;
     flex-flow: column wrap;
-    align-items:center;
-    justify-content:center;
+    align-items: center;
+    justify-content: center;
   }
-  
-  >.header{
-    border:solid 1px red;
+
+  > .header {
+    border: solid 1px red;
   }
-`
+`;
 
 class Invite extends PureComponent {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
 
-    this.sendInvite = this.sendInvite.bind(this)
+    this.sendInvite = this.sendInvite.bind(this);
   }
 
-  sendInvite (email) {
-    axios.get(`${API_URL}${INVITES_SUFIX}?email=${email}`)
-      .then(({ data }) =>
-        toast.success(data.message))
-      .catch(({ response }) => toast.error(response.data.errorMessage))
+  sendInvite(email) {
+    axios
+      .get(`${API_URL}${INVITES_SUFIX}?email=${email}`)
+      .then(({ data }) => toast.success(data.message))
+      .catch(({ response }) => toast.error(response.data.errorMessage));
   }
 
-  render () {
+  render() {
     return (
       <StyledInvite>
-        <div className='content'>
+        <div className="content">
           <Title />
           <InviteForm onRequestInvite={this.sendInvite} />
         </div>
 
         <Footer />
       </StyledInvite>
-
-    )
+    );
   }
 }
 
-export default Invite
+export default Invite;
